1aafde3460c6600b535c81de98de6dee1dd5df75
Eninfosfranco <Eninfosfranco@Eninfosfranco.mare.cl>
2014-07-22 09:57:34 -0400

    creacion de sql de inicio del sistema, creacion de tablas e insert principales

---
+++ protected/_sql/create_db.sql
@@ -0,0 +1,136 @@
+CREATE TABLE tipo_usuario (
+  idtipo_usuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  PRIMARY KEY(idtipo_usuario)
+);
+
+CREATE TABLE estado (
+  idestado INTEGER UNSIGNED NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  abreviatura VARCHAR(15) NOT NULL,
+  PRIMARY KEY(idestado)
+);
+
+CREATE TABLE categoria (
+  idcategoria INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(100) NULL,
+  PRIMARY KEY(idcategoria)
+);
+
+CREATE TABLE subcategoria (
+  idsubcategoria INTEGER UNSIGNED NOT NULL,
+  categoria_idcategoria INTEGER UNSIGNED NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  PRIMARY KEY(idsubcategoria),
+  INDEX subcategoria_FKIndex1(categoria_idcategoria),
+  FOREIGN KEY(categoria_idcategoria)
+    REFERENCES categoria(idcategoria)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+CREATE TABLE usuario (
+  idusuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  tipo_usuario_idtipo_usuario INTEGER UNSIGNED NOT NULL,
+  estado_idestado INTEGER UNSIGNED NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password_2 VARCHAR(200) NOT NULL,
+  nombres VARCHAR(50) NOT NULL,
+  apellidos VARCHAR(50) NOT NULL,
+  sexo VARCHAR(1) NOT NULL,
+  last_login DATETIME NULL,
+  email VARCHAR(150) NULL,
+  PRIMARY KEY(idusuario),
+  INDEX usuario_FKIndex1(estado_idestado),
+  INDEX usuario_FKIndex2(tipo_usuario_idtipo_usuario),
+  FOREIGN KEY(estado_idestado)
+    REFERENCES estado(idestado)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(tipo_usuario_idtipo_usuario)
+    REFERENCES tipo_usuario(idtipo_usuario)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+CREATE TABLE producto (
+  idproducto INTEGER UNSIGNED NOT NULL,
+  estado_idestado INTEGER UNSIGNED NOT NULL,
+  subcategoria_idsubcategoria INTEGER UNSIGNED NOT NULL,
+  nombre VARCHAR(150) NOT NULL,
+  descripcion TEXT NULL,
+  marca VARCHAR(50) NOT NULL,
+  talla VARCHAR(10) NOT NULL,
+  precio INTEGER UNSIGNED NOT NULL,
+  cantidad INTEGER UNSIGNED NOT NULL,
+  img VARCHAR(350) NULL,
+  PRIMARY KEY(idproducto),
+  INDEX producto_FKIndex1(subcategoria_idsubcategoria),
+  INDEX producto_FKIndex2(estado_idestado),
+  FOREIGN KEY(subcategoria_idsubcategoria)
+    REFERENCES subcategoria(idsubcategoria)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(estado_idestado)
+    REFERENCES estado(idestado)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+CREATE TABLE carro (
+  idcarro BIGINT NOT NULL AUTO_INCREMENT,
+  usuario_idusuario INTEGER UNSIGNED NOT NULL,
+  producto_idproducto INTEGER UNSIGNED NOT NULL,
+  fecha DATETIME NOT NULL,
+  cantidad INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(idcarro),
+  INDEX carro_FKIndex1(producto_idproducto),
+  INDEX carro_FKIndex2(usuario_idusuario),
+  FOREIGN KEY(producto_idproducto)
+    REFERENCES producto(idproducto)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(usuario_idusuario)
+    REFERENCES usuario(idusuario)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+CREATE TABLE pedido (
+  idpedido BIGINT NOT NULL AUTO_INCREMENT,
+  estado_idestado INTEGER UNSIGNED NOT NULL,
+  usuario_idusuario INTEGER UNSIGNED NOT NULL,
+  fecha_pedido DATETIME NOT NULL,
+  fecha_entrega DATETIME NULL,
+  PRIMARY KEY(idpedido),
+  INDEX pedido_FKIndex1(usuario_idusuario),
+  INDEX pedido_FKIndex2(estado_idestado),
+  FOREIGN KEY(usuario_idusuario)
+    REFERENCES usuario(idusuario)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(estado_idestado)
+    REFERENCES estado(idestado)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+CREATE TABLE detalle_pedido (
+  iddetalle_pedido BIGINT NOT NULL AUTO_INCREMENT,
+  pedido_idpedido BIGINT NOT NULL,
+  producto_idproducto INTEGER UNSIGNED NOT NULL,
+  cantidad INTEGER UNSIGNED NULL,
+  PRIMARY KEY(iddetalle_pedido),
+  INDEX detalle_pedido_FKIndex1(producto_idproducto),
+  INDEX detalle_pedido_FKIndex2(pedido_idpedido),
+  FOREIGN KEY(producto_idproducto)
+    REFERENCES producto(idproducto)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(pedido_idpedido)
+    REFERENCES pedido(idpedido)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+
---
+++ protected/_sql/insert_db.sql
@@ -0,0 +1,111 @@
+-- -------------------------------------------~
+-- TABLE DATA authitem~
+-- -------------------------------------------~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('mantenedor_usuario','1','acceso completo al mantenedor de usuarios','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('lista_productos','1','acceso para visualizar la lista de productos del sistema','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('carro','1','acceso al carro de compra','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('mantenedor_producto','1','acceso completo al mantenedor de productos','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('mantenedor_detalle_pedido','1','acceso completo a los detalles del pedido','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('mantenedor_pedidos','1','acceso completo a los pedidos realizados','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('pedidos','1','acceso a los pedidos','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('import_csv','1','importación de datos desde archivos .csv','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('mantenedor_subcategorias','1','acceso total al mantenedor de subcategorias','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('mantenedor_categorias','1','acceso total al mantenedor de categorias','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('mantenedor_tipo_usuario','1','acceso total al mantenedor de los tipos de usuario','','N;')~
+INSERT INTO `authitem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('backup','1','acceso a los backup, realizarlo, eliminarlos y restauración','','N;')~
+
+
+
+-- -------------------------------------------~
+-- TABLE DATA categoria~
+-- -------------------------------------------~
+INSERT INTO `categoria` (`idcategoria`,`nombre`) VALUES
+('1','vestimenta')~
+INSERT INTO `categoria` (`idcategoria`,`nombre`) VALUES
+('2','calzado')~
+INSERT INTO `categoria` (`idcategoria`,`nombre`) VALUES
+('3','accesorios')~
+INSERT INTO `categoria` (`idcategoria`,`nombre`) VALUES
+('4','otros')~
+
+
+
+-- -------------------------------------------~
+-- TABLE DATA estado~
+-- -------------------------------------------~
+INSERT INTO `estado` (`idestado`,`nombre`,`abreviatura`) VALUES
+('1','activo','act')~
+INSERT INTO `estado` (`idestado`,`nombre`,`abreviatura`) VALUES
+('5','desactivado','des')~
+INSERT INTO `estado` (`idestado`,`nombre`,`abreviatura`) VALUES
+('2','en espera','espera')~
+INSERT INTO `estado` (`idestado`,`nombre`,`abreviatura`) VALUES
+('3','para retirar','retirar')~
+INSERT INTO `estado` (`idestado`,`nombre`,`abreviatura`) VALUES
+('4','retirado','retirado')~
+
+
+
+-- -------------------------------------------~
+-- TABLE DATA subcategoria~
+-- -------------------------------------------~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('0','4','Sin Familia')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('2','4','Libreria')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('3','3','Accesorio')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('4','1','Tenida')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('5','2','Calzado')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('6','4','Aseo')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('7','1','Ropa')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('8','4','Libros')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('9','4','Lapices')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('10','4','Cartilla')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('11','4','Escritorio')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('12','4','Menaje')~
+INSERT INTO `subcategoria` (`idsubcategoria`,`categoria_idcategoria`,`nombre`) VALUES
+('13','4','Cosméticos')~
+
+
+
+-- -------------------------------------------~
+-- TABLE DATA tipo_usuario~
+-- -------------------------------------------~
+INSERT INTO `tipo_usuario` (`idtipo_usuario`,`nombre`) VALUES
+('1','administrador')~
+INSERT INTO `tipo_usuario` (`idtipo_usuario`,`nombre`) VALUES
+('2','vendedor')~
+INSERT INTO `tipo_usuario` (`idtipo_usuario`,`nombre`) VALUES
+('3','cadete')~
+INSERT INTO `tipo_usuario` (`idtipo_usuario`,`nombre`) VALUES
+('4','cliente')~
+
+
+
+-- -------------------------------------------~
+-- TABLE DATA usuario~
+-- -------------------------------------------~
+INSERT INTO `usuario` (`idusuario`,`tipo_usuario_idtipo_usuario`,`estado_idestado`,`username`,`password_2`,`nombres`,`apellidos`,`sexo`,`last_login`,`email`) VALUES
+('1','1','1','17558919-8','21232f297a57a5a743894a0e4a801fc3','Sebastián','Franco Brantes','H','2014-07-21 12:11:47','seb.frab@gmail.com')~
