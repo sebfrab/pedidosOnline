2f4545bdb2dabb7568196150a5cb4a09049cdb27
Eninfosfranco <Eninfosfranco@Eninfosfranco.mare.cl>
2014-07-25 12:10:27 -0400

    ajuste para upload de archivos

---
+++ upload/importcsv/productos2.php
@@ -0,0 +1,14 @@
+<?php
+                $paramsArray = array(
+                    "table"=>"producto",
+                    "delimiter"=>";",
+		    "textDelimiter"=>"\"",
+                    "mode"=>3,
+                    "perRequest"=>10,
+                    "csvKey"=>"1",
+                    "tableKey"=>"idproducto",
+                    "columns"=>array(
+                        "idproducto"=>1, "estado_idestado"=>2, "subcategoria_idsubcategoria"=>3, "nombre"=>4, "descripcion"=>5, "marca"=>6, "talla"=>7, "precio"=>8, "cantidad"=>9, "img"=>"", 
+                    ),
+                );
+            ?>
