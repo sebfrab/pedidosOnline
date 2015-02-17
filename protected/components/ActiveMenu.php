5590b7bf8d76d91b8fe86dc85c53b04b49899457
Eninfosfranco <Eninfosfranco@Eninfosfranco.mare.cl>
2015-02-17 10:02:40 -0300

    ajustes del menú automático, opciones contacto y ayuda al momento de no estar logeado.

--- protected/components/ActiveMenu.php
+++ protected/components/ActiveMenu.php
@@ -6,21 +6,28 @@ class ActiveMenu extends CMenu
 {
     public function init()
     {
-        
+        $auth=Yii::app()->authManager;
         $items = Categoria::model()->findAll();
         
         $count=0;
-        foreach ($items as $item){
-            $this->items[$count] = array('label'=>strtoupper($item->nombre).' <b class="caret"></b>', 'url'=>'#', 'linkOptions'=>array(
-                                                            'class'=>'dropdown-toggle',
-                                                            'data-toggle'=>'dropdown',
-                                                        ),);
-            foreach($item->subcategoria as $sub){
-                $this->items[$count]["items"][] = array('label'=>$sub->nombre, 'url'=>array('/listProductos/index/'.$sub->idsubcategoria));
+        if($auth->checkAccess('lista_productos',Yii::app()->user->id)){
+            foreach ($items as $item){
+                $this->items[$count] = array('label'=>strtoupper($item->nombre).' <b class="caret"></b>', 'url'=>'#', 'linkOptions'=>array(
+                                                                'class'=>'dropdown-toggle',
+                                                                'data-toggle'=>'dropdown',
+                                                            ),
+                    'visible'=>$auth->checkAccess('lista_productos',Yii::app()->user->id));
+                foreach($item->subcategoria as $sub){
+                    $this->items[$count]["items"][] = array('label'=>$sub->nombre, 'url'=>array('/listProductos/index/'.$sub->idsubcategoria));
+                }
+                $count++; 
             }
-            $count++; 
         }
-        
+        if(Yii::app()->user->isGuest){
+            $this->items[$count] = array('label'=>'AYUDA', 'url'=>array('/site/support'),'visible'=>Yii::app()->user->isGuest);
+            $count++; 
+            $this->items[$count] = array('label'=>'CONTACTO', 'url'=>array('/site/index#contacto'),'visible'=>Yii::app()->user->isGuest);
+        }        
     }
 }
 ?>
